625993f197ab2260e7a509bde7c4ec9a1aa5fe31
bashton <14bashton@gmail.com>
2023-09-10 22:29:04 -0600

    added some base styling for headers

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -7,9 +7,9 @@ export const Editor = () => {
 
     // const markdown = '***abc*** abc';
     return (
-        <>
+        <div className='flex flex-col content-start text-left'>
             <textarea className='editor w-full h-100vh font-mono' wrap="hard" value={text} onChange={(e) => setText(e.target.value)}/>
             <Markdown value={text} gfm/>
-        </>
+        </div>
     );
 }
