dd214b671704dd3bd9e87b27e7840bf40ccd1fde
bashton <14bashton@gmail.com>
2023-09-11 13:29:53 -0600

    working markdown editor, no ui

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -7,9 +7,11 @@ export const Editor = () => {
 
     // const markdown = '***abc*** abc';
     return (
-        <div className='flex flex-col content-start text-left'>
-            <textarea className='editor w-full h-100vh font-mono' wrap="hard" value={text} onChange={(e) => setText(e.target.value)}/>
-            <Markdown value={text} gfm/>
+        <div className='flex flex-row content-start text-left h-screen'>
+            <textarea className='editor font-mono border-2 h-5/6 w-1/2' wrap="hard" value={text} onChange={(e) => setText(e.target.value)}/>
+            <div className='w-1/2 h-5/6 overflow-y-scroll'>
+                <Markdown value={text} gfm/>
+            </div>
         </div>
     );
 }
