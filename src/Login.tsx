9ef2f5b525a9e0c428b7d7968b7647d07e414436
bashton <14bashton@gmail.com>
2023-09-29 19:08:05 -0600

    login error messages

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,6 +6,8 @@ import { auth } from "../lib/firebase";
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrectPassword, setIncorrectPassword] = useState(false);
+    const [tooManyRequests, setTooManyRequests] = useState(false);
     
     const navigate = useNavigate();
     const provider = new GoogleAuthProvider();
@@ -18,6 +20,19 @@ export const Login = () => {
         signInWithEmailAndPassword(auth, email ? email : "", password ? password : "")
                 .then(({user}) => {
                     console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.error('Code: ', errorCode, '\nMessage: ', errorMessage);
+                    if (errorCode === 'auth/wrong-password') {
+                        setIncorrectPassword(true);
+                        setTooManyRequests(false);
+                    }
+                    if (errorCode === 'auth/too-many-requests') {
+                        setTooManyRequests(true);
+                        setIncorrectPassword(false);
+                    }
                 });
     };
 
@@ -29,29 +44,32 @@ export const Login = () => {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST">
-                <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 text-left">Email address</label>
-                    <div className="mt-2">
-                        <input onChange={(e) => {setEmail(e.target.value)}} value={email} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="flex items-center justify-between">
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <div className="text-sm">
-                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                <form onSubmit={handleSubmit} action="#" className="space-y-6" method="POST">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 text-left">Email address</label>
+                        <div className="mt-2">
+                            <input onChange={(e) => {setEmail(e.target.value)}} value={email} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                         </div>
                     </div>
-                    <div className="mt-2">
-                        <input onChange={(e) => {setPassword(e.target.value)}} value={password} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                </div>
 
-                <div>
-                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
-                </div>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input onChange={(e) => {setPassword(e.target.value)}} value={password} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        <p hidden={!incorrectPassword} className="text-red-600 text-left">Incorrect email or password. Please Try again.</p>
+                        <p hidden={!tooManyRequests} className="text-red-600 text-left">Too many incorrect attempts. Your account has been locked. Try again later or reset password.</p>
+                    </div>
+                    
+
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
+                    </div>
                 </form>
                 <hr className="mt-10"/>
                 <button onClick={() => signInWithRedirect(auth, provider)} type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2 mt-10 w-full">
